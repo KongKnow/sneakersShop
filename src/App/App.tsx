49c015af06123ee95745540895d5fcb7ec857e69
KongKnow <egorzac@gmail.com>
2023-11-19 12:27:01 +0200

    loading curtain

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -12,36 +12,53 @@ import { useAppDispatch } from "../redux/store";
 import { setUser } from "../redux/userSlice/userSlice";
 import ChangePassword from "../components/Profile/ChangePassword";
 import { TypeInitialStateUser } from "../redux/userSlice/userSlice";
+import logo from '../assets/img/logo.svg'
 
 const App: FC = () => {
     const dispatch = useAppDispatch()
     const [showCart, setShowCart] = useState(false)
-    
+    const [isLoaded, setIsLoaded] = useState(false)
+    const [isPageLoaded, setIsPageLoaded] = useState(false)
+
     const handleClick = () => {
         setShowCart(showCart => !showCart)
         document.body.classList.toggle('active')
     }
 
     useEffect(() => {
+        setIsLoaded(true)
         const user: TypeInitialStateUser = JSON.parse(localStorage.getItem('ss-account') || '{}')
         if(!user) return
         dispatch(setUser(user))
     }, [])
 
+    useEffect(() => {
+        if (isLoaded) {
+            setIsPageLoaded(true);
+        }
+    }, [isLoaded]);
+
     return (
-        <section className="container">
-            <Header handleClick={handleClick}/>
-            <Routes>
-                <Route path="/" element={<Home/>}/>
-                <Route path="/favorites" element={<Favorites/>}/>
-                <Route path="/profile" element={<Profile/>}/>
-                <Route path="/product/:id" element={<ProductsPage/>}/>
-                <Route path="/sign-up" element={<SignUpForm/>}/>
-                <Route path="/log-in" element={<LogInForm/>}/>
-                <Route path="/change-password" element={<ChangePassword/>}/>
-            </Routes>
-            <Cart handleClick={handleClick} active={showCart}/>
-        </section>
+        <>
+            
+            <div className={isPageLoaded ? "curtain active" : "curtain"}>
+                <img src={logo} alt="" />
+            </div>
+            
+            <section className="container">
+                <Header handleClick={handleClick}/>
+                <Routes>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="/favorites" element={<Favorites/>}/>
+                    <Route path="/profile" element={<Profile/>}/>
+                    <Route path="/product/:id" element={<ProductsPage/>}/>
+                    <Route path="/sign-up" element={<SignUpForm/>}/>
+                    <Route path="/log-in" element={<LogInForm/>}/>
+                    <Route path="/change-password" element={<ChangePassword/>}/>
+                </Routes>
+                <Cart handleClick={handleClick} active={showCart}/>
+            </section>
+        </>
     );
 };
 
